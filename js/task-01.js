47f1ca0cefcd3953a4d6785037f0d49879787fa0
Yevhen-L <ujeen2101@gmail.com>
2023-07-12 15:27:11 +0300

    Update task-01.js
    add code

---
+++ js/task-01.js
@@ -0,0 +1,12 @@
+const categoriesList = document.querySelector("#categories");
+const categoriesItems = categoriesList.querySelectorAll("item");
+const categoriesCount = categoriesItems.length;
+  console.log(`Number of categories: ${categoriesCount}`);
+
+categoriesItems.forEach((item) => {
+
+  const categoriesTitle = item.querySelector("h2").textContent;
+  const categoriesElemntsCounts = querySelectorAll("li").length;
+  console.log(`Category: ${categoriesTitle}`);
+  console.log(`Elements: ${categoriesElemntsCounts}`);
+});
