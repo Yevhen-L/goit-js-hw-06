3d1777c02800651f5a7e962b2f081a9994e6ce51
Yevhen-L <ujeen2101@gmail.com>
2023-07-19 22:03:15 +0300

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const loginForm = document.querySelector('.login-form');
 
+loginForm.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const formData = new FormData(loginForm);
+  const formDataObject = {};
+  formData.forEach((value, key) => {
+    formDataObject[key] = value;
+  });
+  if (formDataObject.email === '' || formDataObject.password === '') {
+    alert('All fields must be filled');
+  } else {
+       console.log(formDataObject);
+        loginForm.reset();
+  }
+});
