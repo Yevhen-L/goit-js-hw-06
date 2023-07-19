1a3ea61b2c494a4be1c059e4e440b0458e07d200
Yevhen-L <ujeen2101@gmail.com>
2023-07-19 21:15:07 +0300

    7 task

---
+++ js/task-07.js
@@ -0,0 +1,7 @@
+const fontSizeControl = document.getElementById('font-size-control');
+const textSpan = document.getElementById('text');
+function updateFontSize() {
+  const fontSize = fontSizeControl.value + 'px';
+  textSpan.style.fontSize = fontSize;
+}
+fontSizeControl.addEventListener('input', updateFontSize);
