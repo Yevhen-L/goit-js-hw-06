3543177036485c418bebb3dd014a46735ac0ac8c
Yevhen-L <ujeen2101@gmail.com>
2023-07-20 18:07:00 +0300

    new code for task 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,10 +6,21 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
-const ul = document.querySelector("ul#ingredients");
-ingredients.forEach(function(ingredient) {
-  const li = document.createElement("li");
-  li.textContent = ingredient;
-  li.classList.add("item");
-  ul.appendChild(li);
-});
+
+// const ul = document.querySelector("ingredients");
+// ingredients.forEach(function(ingredient) {
+//   const li = document.createElement("li");
+//   li.textContent = ingredient;
+//   li.classList.add("item");
+//   ul.appendChild(li);
+// });
+
+
+const ulElements = document.createElement("ul");
+for (const ingredient of ingredients) {
+  const liElement = document.createElement("li");
+    liElement.textContent = ingredient;
+    liElement.classList.add("item");
+    ulElements.appendChild(liElement);
+}
+document.body.appendChild(ulElements);
